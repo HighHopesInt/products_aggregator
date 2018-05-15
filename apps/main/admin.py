9dc2164225c5a22e97a309c3d4e6e80411f4e0a8
ukhryab <mihail.sibirev@mail.ru>
2018-05-15 18:45:25 +0300

    Change admin models representation.

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-
+from mptt.admin import MPTTModelAdmin
 from .models import Category, Product, Brand, Color
 
 # Register your models here.
 
-admin.site.register([Category, Product, Brand, Color])
+
+class CategoryAdmin(MPTTModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register([Product, Brand, Color])
+admin.site.register(Category, CategoryAdmin)
