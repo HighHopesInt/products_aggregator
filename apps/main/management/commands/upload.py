21f36fabf7eee8dbe6b04aea55f57444c89b13cf
leocoan <drdlemmer@gmail.com>
2019-07-24 11:56:45 +0300

    Init command class

--- apps/main/management/commands/upload.py
+++ apps/main/management/commands/upload.py
@@ -1,20 +1,7 @@
 from django.core.management.base import BaseCommand
-from apps.main.tasks import parse
 
 
-class Command(BaseCommand):
-
-    help = 'Uploaded file in DataBase'
-
-    def handle(self, *args, **options):
-        if options['file']:
-            parse()
-
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '-f',
-            '--file',
-            action='store_true',
-            default=False,
-            help='The upload file'
-        )
+def upload_file():
+    class Command(BaseCommand):
+        def handle(self, *args, **options):
+            upload_file()
