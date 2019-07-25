424c13e25519226a59637201a16919050a5e26f5
leocoan <drdlemmer@gmail.com>
2019-07-25 16:10:41 +0300

    Add command upload

--- apps/main/management/commands/upload.py
+++ apps/main/management/commands/upload.py
@@ -1,7 +1,29 @@
+import tempfile
 from django.core.management.base import BaseCommand
+from apps.main.models import UploadedFile
 
 
-def upload_file():
-    class Command(BaseCommand):
-        def handle(self, *args, **options):
-            upload_file()
+class Command(BaseCommand):
+    help = 'Upload .csv file in DB'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--file',
+                            '-f',
+                            help='File to be upload',
+                            type=str)
+
+    def handle(self, *args, **options):
+        if options['file']:
+            file = options['file']
+            data = open(file, 'r').read()
+            temp_file = tempfile.NamedTemporaryFile(mode='w',
+                                                    delete=False)
+            temp_file.write(data)
+            upload_file = UploadedFile()
+            print('Done')
+            upload_file.file = temp_file.name
+            upload_file.save()
+            print(temp_file.name)
+            temp_file.close()
+        else:
+            print('You do\'t write arguments')
