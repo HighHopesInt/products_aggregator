ccfdfe9bd7df287e681fd33fc3bc01a15c2b5192
leocoan <drdlemmer@gmail.com>
2019-07-26 13:52:52 +0300

    Add upload files in directory and multiplue files

--- apps/main/management/commands/upload.py
+++ apps/main/management/commands/upload.py
@@ -1,21 +1,32 @@
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.management.base import BaseCommand
 from apps.main.models import UploadedFile
+import os
 
 
 class Command(BaseCommand):
     help = 'Upload .csv file in DB'
 
     def add_arguments(self, parser):
-        parser.add_argument('--file',
-                            '-f',
-                            help='File to be upload',
-                            type=str)
+        parser.add_argument('file', nargs='*', type=str)
 
     def handle(self, *args, **options):
-        if options['file']:
-            with open(options['file'], 'rb') as f:
-                data_file = SimpleUploadedFile(name=f.name, content=f.read())
-            UploadedFile.objects.all().create(file=data_file)
-        else:
-            print('You do\'t write arguments')
+        try:
+            for item in options['file']:
+                if os.path.isdir(item):
+                    for filename in os.listdir(item):
+                        with open(item + filename, 'rb') as f:
+                            data_file = SimpleUploadedFile(name=f.name,
+                                                           content=f.read())
+                        UploadedFile.objects.all().create(file=data_file)
+                else:
+                    with open(item, 'rb') as f:
+                        data_file = SimpleUploadedFile(name=f.name,
+                                                       content=f.read())
+                    UploadedFile.objects.all().create(file=data_file)
+        except FileNotFoundError:
+            print('Not Found File')
+        except ValueError:
+            print('Something went wrong')
+        except TypeError:
+            print('Something went wrong with type')
