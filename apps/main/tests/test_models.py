4667ab1d720c144cfb35bca5c11220795afe70ce
leocoan <drdlemmer@gmail.com>
2019-07-30 15:41:32 +0300

    Add test for description for model Product

--- apps/main/tests/test_models.py
+++ apps/main/tests/test_models.py
@@ -2,11 +2,30 @@ from django.test import TestCase
 from apps.main.models import Product, Category
 
 
-# class ProductModelTest(TestCase):
-#
-#     @classmethod
-#     def setUpTestData(cls):
-#         Product.objects.create()
+class ProductModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Product.objects.create(description='Example description',
+                               meta_description='Meta desc',
+                               short_description='Short desc',
+                               title='It is title',
+                               meta_title='It is title',
+                               material='Plastic',
+                               size='45 EU',
+                               free_shipping=True,
+                               available=False,
+                               price=45.6)
+
+    def test_description_label(self):
+        product = Product.objects.get(id=1)
+        field_label = product._meta.get_field('description').verbose_name
+        self.assertEquals(field_label, 'description')
+
+    def test_description_lenght(self):
+        prodcut = Product.objects.get(id=1)
+        max_length = prodcut._meta.get_field('name').max_length
+        self.assertEquals(max_length, 1000)
 
 
 class CategoryModelTest(TestCase):
