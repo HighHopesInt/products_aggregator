04ccd83e423ce78fa6495c8b1a12de2db1a4540a
leocoan <drdlemmer@gmail.com>
2019-08-02 15:29:28 +0300

    Add tets for admin upload files

--- apps/main/tests/test_views.py
+++ apps/main/tests/test_views.py
@@ -1,9 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
+from django.contrib.admin.sites import AdminSite
 from mixer.backend.django import mixer
+from django.conf import settings
 
 from apps.main.models import UploadedFile, Product, Category
 from apps.main.admin import UploadedFileAdmin
+
+
+class MockSuperUser:
+    def has_perm(self, perm):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_staff(self):
+        return True
+
+
+request_factory = RequestFactory()
+request = request_factory.get('/admin')
+request.user = MockSuperUser()
+request.files = [settings.MEDIA_ROOT + 'uploaded_files/WOMEN_SHOES.csv',
+                 settings.MEDIA_ROOT + 'uploaded_files/file.txt']
 
 
 class ProductListViewTest(TestCase):
@@ -25,3 +45,18 @@ class ProductListViewTest(TestCase):
         resp = self.client.get(reverse('index'))
         self.assertEquals(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'catalog.html')
+
+
+class TestUploadFile(TestCase):
+    def setUp(self):
+        site = AdminSite()
+        self.admin = UploadedFileAdmin(UploadedFile, site)
+
+    def test_upload_status_code(self):
+        proceed = self.admin.multiple_upload_files(request)
+        self.assertEquals(proceed.status_code, 200)
+
+    def test_upload_files(self):
+        for item in request.files:
+            with open(item) as f:
+                self.client.post('/admin/main/uploadedfile/add/', {'form': f})
