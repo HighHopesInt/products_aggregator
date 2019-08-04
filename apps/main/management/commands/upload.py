51f70659d25bf0d726308d6f17cffcd30de4d528
Eduard Lemmer <34683354+leocoan@users.noreply.github.com>
2019-08-04 22:50:18 +0300

    Upload file from manage.py (#13)
    * Add command upload
    
    * Init command class
    
    * Add command upload
    
    * Replace standrat tempfile on django simpleuploadfile
    
    * Add upload files in directory and multiplue files
    
    * Move import os in start of file
    
    * Add check on absence files or directory
    
    * Add message about success upload files
    
    * Replace print() to stdout

---
+++ apps/main/management/commands/upload.py
@@ -0,0 +1,36 @@
+import os
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.management.base import BaseCommand
+from apps.main.models import UploadedFile
+
+
+class Command(BaseCommand):
+    help = 'Upload .csv file in DB'
+
+    def add_arguments(self, parser):
+        parser.add_argument('file', nargs='*', type=str)
+
+    def handle(self, *args, **options):
+        try:
+            if not options['file']:
+                raise ValueError
+            for item in options['file']:
+                if os.path.isdir(item):
+                    for filename in os.listdir(item):
+                        with open(item + filename, 'rb') as f:
+                            data_file = SimpleUploadedFile(name=f.name,
+                                                           content=f.read())
+                        UploadedFile.objects.all().create(file=data_file)
+                        self.stdout.write(item + filename + ' upload success')
+                else:
+                    with open(item, 'rb') as f:
+                        data_file = SimpleUploadedFile(name=f.name,
+                                                       content=f.read())
+                    UploadedFile.objects.all().create(file=data_file)
+                    self.stdout.write(item + ' upload success')
+        except FileNotFoundError:
+            print('Not Found File')
+        except ValueError:
+            print('Something went wrong')
+        except TypeError:
+            print('Something went wrong with type')
