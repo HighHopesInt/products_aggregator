a6f7d00bc6fe62c1bae4ebbddbbe758899ca57ca
leocoan <drdlemmer@gmail.com>
2019-08-13 12:17:35 +0300

    Add redirect on file list and add label for checkbox input

--- apps/scraper/forms.py
+++ apps/scraper/forms.py
@@ -2,5 +2,8 @@ from django import forms
 
 
 class ChoseSiteForm(forms.Form):
-    site_one = forms.BooleanField(required=False)
-    site_two = forms.BooleanField(required=False)
+    site_one = forms.BooleanField(required=False,
+                                  label='https://www.francosarto.com/en-US')
+    site_two = forms.BooleanField(required=False,
+                                  label=('https://www.saksfifthavenue.com'
+                                         '/Men/Shoes'))
--- apps/scraper/views.py
+++ apps/scraper/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .forms import ChoseSiteForm
 
 
 def chose_site_admin(request):
     form = ChoseSiteForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            return HttpResponseRedirect('/admin/main/uploadedfile/')
     context = {'form': form}
     return render(request, 'scraper/chose_sites.html', context)
