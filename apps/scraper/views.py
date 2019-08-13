e21ce7947531cf14a15a14a5b5713915336665e7
leocoan <drdlemmer@gmail.com>
2019-08-13 12:47:26 +0300

    Add messages for checkboxs debug information

--- apps/scraper/views.py
+++ apps/scraper/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .forms import ChoseSiteForm
 
@@ -7,6 +8,12 @@ def chose_site_admin(request):
     form = ChoseSiteForm(request.POST)
     if request.method == 'POST':
         if form.is_valid():
+            # Next two messages is debug information and don't hit in final
+            # version
+            messages.success(request, str(request.POST.get('site_one',
+                                                           'False')))
+            messages.success(request, str(request.POST.get('site_two',
+                                                           'False')))
             return HttpResponseRedirect('/admin/main/uploadedfile/')
     context = {'form': form}
     return render(request, 'scraper/chose_sites.html', context)
