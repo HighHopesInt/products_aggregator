c9795ab4b5aa1c43f945f43c2ec1543119b93071
leocoan <drdlemmer@gmail.com>
2019-08-13 16:26:47 +0300

    Edit scraper view form

--- apps/scraper/forms.py
+++ apps/scraper/forms.py
@@ -5,7 +5,8 @@ from .models import Site
 class ChoseSiteForm(forms.Form):
     for site in range(len(Site.objects.all())):
         locals()['site_' + str(site + 1)] = forms.BooleanField(
-            label=(Site.objects.get(id=site + 1).main_url
-                   + Site.objects.get(id=site + 1).slug),
+            label=(Site.objects.get(id=site + 1).title +
+                   ' (' + Site.objects.get(id=site + 1).main_url +
+                   Site.objects.get(id=site + 1).slug[:20] + '...)'),
             required=False
         )
