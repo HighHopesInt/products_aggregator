8ab4f04c8fd23112b65ed03eb902eef43f854da2
leocoan <drdlemmer@gmail.com>
2019-08-13 15:44:13 +0300

    Edit site model

--- apps/scraper/models.py
+++ apps/scraper/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Site(models.Model):
     title = models.CharField(max_length=50, verbose_name='Title')
     main_url = models.CharField(verbose_name='Main url', max_length=50)
-    slug = models.SlugField(verbose_name='Slug')
+    slug = models.CharField(verbose_name='Pseudo slug', max_length=100)
 
     def __str__(self):
         return self.title
