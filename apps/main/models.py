6dd5aba252641807efcac6728041cd4c3222c820
leocoan <drdlemmer@gmail.com>
2019-08-29 11:24:13 +0300

    Fix return issue

--- apps/main/models.py
+++ apps/main/models.py
@@ -95,7 +95,9 @@ class Product(models.Model):
         if not self.image_url:
             return False
         correct_ext = ['.jpeg', '.jpg', '.png']
-        return any([str(self.image_url).endswith(ext) for ext in correct_ext])
+        if not any([str(self.image_url).endswith(ext)
+                    for ext in correct_ext]):
+            return False
         r = requests.get(self.image_url, headers=headers)
         return r.status_code == requests.codes.ok
 
