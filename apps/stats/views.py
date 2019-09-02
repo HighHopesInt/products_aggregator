9c7b5d22c95d7d812b6b456f09f9c91c6ad8cfd8
leocoan <drdlemmer@gmail.com>
2019-09-02 12:17:33 +0300

    Add count of all products and popular brand

--- apps/stats/views.py
+++ apps/stats/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Count
+
+from apps.main.models import Product, Brand
 
 
 def stats(request):
-    return render(request, 'stats.html', {})
+    count = Product.objects.all().count()
+    top_brand = (Brand.objects.all().annotate(num=Count('products'))
+                 .order_by('-num').values_list('name', 'num'))[:5]
+    context = {
+        'count': count,
+        'top_brand': top_brand,
+    }
+    return render(request, 'stats.html', context)
