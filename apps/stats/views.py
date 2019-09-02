ae5b7c807b69b8c49e7f5ff4ae7afc3f85b03a3b
leocoan <drdlemmer@gmail.com>
2019-09-02 18:12:17 +0300

    Add some stats

--- apps/stats/views.py
+++ apps/stats/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.db.models import Count
+from django.db.models import Count, FloatField
+from django.db.models.functions import Cast
 
-from apps.main.models import Product, Brand
+from apps.main.models import Product, Brand, Color, Retailer
 
 
 def stats(request):
@@ -15,10 +16,20 @@ def stats(request):
                        .order_by('-price')
                        .values_list('title', 'brand__name', 'color__name',
                                     'price', 'sale_price').distinct())[:3]
+    per_ratio = (Color.objects.all().annotate(
+                 num=Cast(Count('product') / count * 100, FloatField()))
+                 .order_by('name').values_list('name', 'num'))
+    famous_stat = (Product.objects.filter(description__icontains='Famous')
+                   .count())
+    ret_stat = (Retailer.objects.all().annotate(prod_of=Count('product'))
+                .values_list('name', 'brand_of', 'prod_of'))
     context = {
         'count': count,
         'top_brand': top_brand,
         'top_male_boot': top_male_boot,
-        'top_female_boot': top_female_boot
+        'top_female_boot': top_female_boot,
+        'per_ratio': per_ratio,
+        'famous_word': famous_stat,
+        'retailer_stat': ret_stat,
     }
     return render(request, 'stats.html', context)
