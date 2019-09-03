b07e53df9e2f74344fa512b381f2d3565944983b
leocoan <drdlemmer@gmail.com>
2019-09-03 12:37:34 +0300

    Add summary by retailer

--- apps/stats/views.py
+++ apps/stats/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.db.models import Count, FloatField
 from django.db.models.functions import Cast
 
-from apps.main.models import Product, Brand, Color, Retailer
+from apps.main.models import Product, Brand, Color
 
 
 def stats(request):
     count = Product.objects.all().count()
-    top_brand = (Brand.objects.all().annotate(num=Count('products'))
+    top_brand = (Brand.objects.annotate(num=Count('products'))
                  .order_by('-num').values_list('name', 'num'))[:5]
     top_male_boot = (Product.objects.filter(gender='Men').order_by('-price')
                      .values_list('title', 'brand__name', 'color__name',
@@ -16,13 +16,15 @@ def stats(request):
                        .order_by('-price')
                        .values_list('title', 'brand__name', 'color__name',
                                     'price', 'sale_price').distinct())[:3]
-    per_ratio = (Color.objects.all().annotate(
+    per_ratio = (Color.objects.annotate(
                  num=Cast(Count('product') / count * 100, FloatField()))
                  .order_by('name').values_list('name', 'num'))
     famous_stat = (Product.objects.filter(description__icontains='Famous')
                    .count())
-    ret_stat = (Retailer.objects.all().annotate(prod_of=Count('product'))
-                .values_list('name', 'brand_of', 'prod_of'))
+    ret_stat = (Product.objects.values('retailer').
+                annotate(prod_of=Count('id'),
+                         brand_of=Count('brand', distinct=True))
+                .values_list('retailer__name', 'brand_of', 'prod_of'))
     context = {
         'count': count,
         'top_brand': top_brand,
