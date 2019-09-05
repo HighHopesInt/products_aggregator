6bddebdda1153c51a8e1a989ed9d53cb4d566667
leocoan <drdlemmer@gmail.com>
2019-09-05 13:55:38 +0300

    Add row for other colors

--- apps/stats/views.py
+++ apps/stats/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.db.models import Count, FloatField, Sum, ExpressionWrapper
+from django.db.models import Count, FloatField, ExpressionWrapper
 
 from apps.main.models import Product, Brand, Color
 
 
 def stats(request):
     products_count = Product.objects.all().count()
-    print(products_count)
     top_brand = (Brand.objects.annotate(num=Count('products'))
                  .order_by('-num').values_list('name', 'num'))[:5]
     top_male_boot = (Product.objects.filter(gender__iexact='men')
@@ -21,6 +20,8 @@ def stats(request):
                  (Count('product') * 100 / products_count,
                   output_field=FloatField()))
                  .values_list('name', 'num').order_by('-num'))
+    other_percent = [percent[1] for percent in per_ratio if percent[1] < 1.0]
+    other_percent = round(sum(other_percent), 4)
     famous_stat = (Product.objects.filter(description__icontains='famous')
                    .count())
     ret_stat = (Product.objects.values('retailer').
@@ -28,8 +29,6 @@ def stats(request):
                          brand_of=Count('brand', distinct=True))
                 .order_by('retailer__name')
                 .values_list('retailer__name', 'brand_of', 'prod_of'))
-    per_ratio_all = per_ratio.aggregate(Sum('num'))
-    print(per_ratio_all)
     context = {
         'count': products_count,
         'top_brand': top_brand,
@@ -38,5 +37,6 @@ def stats(request):
         'per_ratio': per_ratio,
         'famous_word': famous_stat,
         'retailer_stat': ret_stat,
+        'other_percent': other_percent,
     }
     return render(request, 'stats.html', context)
