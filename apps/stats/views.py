0c3a34d8dfc6aa69c295a4fdc20110056806fa30
leocoan <drdlemmer@gmail.com>
2019-09-06 16:53:59 +0300

    Add error handler and floatformat

--- apps/stats/views.py
+++ apps/stats/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.db.models import Count, FloatField, ExpressionWrapper
 
-from apps.main.models import Product, Brand, Color
+from apps.main.models import Product
 
 
 def stats(request):
     products_count = Product.objects.all().count()
-    top_brand = (Brand.objects.annotate(num=Count('products'))
-                 .order_by('-num').values_list('name', 'num'))[:5]
+    top_brand = (Product.objects.values('brand')
+                 .annotate(num=Count('title'))
+                 .order_by('-num').values_list('brand__name', 'num'))[:5]
     top_male_boot = (Product.objects.filter(gender__iexact='men')
                      .order_by('-price')
                      .values_list('title', 'brand__name', 'color__name',
@@ -16,11 +17,14 @@ def stats(request):
                        .order_by('-price')
                        .values_list('title', 'brand__name', 'color__name',
                                     'price', 'sale_price').distinct())[:3]
-    per_ratio = (Color.objects.annotate(num=ExpressionWrapper
-                 (Count('product') * 100 / products_count,
-                  output_field=FloatField()))
-                 .values_list('name', 'num').order_by('-num'))
-    other_percent = [percent[1] for percent in per_ratio if percent[1] < 1.0]
+    per_ratio = (Product.objects.values('color').annotate(
+                    num=ExpressionWrapper(
+                        Count('title') * 100 / float(products_count),
+                        output_field=FloatField()))
+                 .values_list('color__name', 'num').order_by('-num'))
+    other_percent = [percent[1] for percent in per_ratio
+                     if not isinstance(percent[1], type(None))
+                     and percent[1] < 1.0]
     other_percent = round(sum(other_percent), 4)
     famous_stat = (Product.objects.filter(description__icontains='famous')
                    .count())
