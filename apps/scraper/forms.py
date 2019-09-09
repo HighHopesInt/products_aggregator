77dba52a59d2403b138039fe24b1fd8ad05ffea1
leocoan <drdlemmer@gmail.com>
2019-09-09 13:58:01 +0300

    Rename field in site model

--- apps/scraper/forms.py
+++ apps/scraper/forms.py
@@ -8,7 +8,7 @@ class ChooseSiteForm(forms.Form):
         locals()['site_' + str(site.id)] = forms.BooleanField(
             label=(site.title + ' (' + site.url[:50] + '...)'),
             required=False, widget=forms.CheckboxInput(attrs={
-                'value': site.name_for_crauler,
+                'value': site.name_for_crawler,
             })
         )
         set_field_html_name(locals()['site_' + str(site.id)], 'site')
--- apps/scraper/models.py
+++ apps/scraper/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Site(models.Model):
     title = models.CharField(verbose_name='Title', max_length=50)
     url = models.CharField(verbose_name='Url', max_length=150)
-    name_for_crauler = models.CharField(verbose_name='Main slug',
+    name_for_crawler = models.CharField(verbose_name='Name crawler',
                                         max_length=40,
                                         null=True)
 
